9eb667b8ef7263da7dad4ddf3c2c69f16d53237f
Cesar Oliveira <cesarsdoliveira@gmail.com>
2020-12-22 19:12:01 +1100

    Creating model System Requirements and initial test validation

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  it { is_expected.to validate_presence_of(:name) }
-  it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+  it { should validate_presence_of(:name) }
+  it { should validate_uniqueness_of(:name).case_insensitive }
 end
