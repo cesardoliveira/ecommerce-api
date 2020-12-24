fa1afe4f7fb68f030cade02ad547c9547161ee4b
Cesar Oliveira <cesarsdoliveira@gmail.com>
2020-12-24 12:09:41 +1100

    Creating model ProductCategory and integrated rspec test with Product and Category

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  subject { build(:product) } 
+  subject { build(:product) }
   
+  it { should belong_to(:productable) }
+
   it { should validate_presence_of(:name) }
   it { should validate_uniqueness_of(:name).case_insensitive }
   it { should validate_presence_of(:description) }
   it { should validate_presence_of(:price) }
   it { should validate_numericality_of(:price).is_greater_than(0) }
-  it { should belong_to(:productable) } 
+
+  it { should have_many(:product_categories).dependent(:destroy) }
+  it { should have_many(:categories).through(:product_categories) }
 end
