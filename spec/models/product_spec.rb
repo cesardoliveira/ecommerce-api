fb0faffbd05e59e18a30ab7c92149d7b0ab8156a
Cesar Oliveira <cesarsdoliveira@gmail.com>
2020-12-29 11:13:20 +1100

    Adding image to Product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -10,7 +10,9 @@ RSpec.describe Product, type: :model do
   it { should validate_presence_of(:description) }
   it { should validate_presence_of(:price) }
   it { should validate_numericality_of(:price).is_greater_than(0) }
+  it { should validate_presence_of(:image) }
 
   it { should have_many(:product_categories).dependent(:destroy) }
   it { should have_many(:categories).through(:product_categories) }
+
 end
