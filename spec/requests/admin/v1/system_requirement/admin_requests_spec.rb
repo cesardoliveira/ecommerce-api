b9771b8fb42b9bc741a8b1318c27f69b53bba33f
Cesar Oliveira <cesarsdoliveira@gmail.com>
2020-12-31 12:58:27 +1100

    Creating CRUD of Coupon with validation tests

--- spec/requests/admin/v1/system_requirement/admin_requests_spec.rb
+++ spec/requests/admin/v1/system_requirement/admin_requests_spec.rb
@@ -35,7 +35,7 @@ RSpec.describe "Admin::V1::SystemRequirements as :admin", type: :request do
         post url, headers: auth_header(user), params: system_requirement_params
         expected_system_requirement = SystemRequirement.last
         .as_json(only: %i(id name operational_system storage processor memory video_board))
-        expect(body_json['expected_system_requirement']) == expected_system_requirement
+        expect(body_json['system_requirement']) == expected_system_requirement
       end
 
       it 'returns success status' do
@@ -45,23 +45,23 @@ RSpec.describe "Admin::V1::SystemRequirements as :admin", type: :request do
     end
 
     context "with invalid params" do
-      let(:system_requirements_invalid_params) do 
+      let(:system_requirement_invalid_params) do 
         { system_requirement: attributes_for(:system_requirement, name: nil) }.to_json
       end
   
       it 'does not add a new SystemRequirement' do
         expect do
-          post url, headers: auth_header(user), params: system_requirements_invalid_params    
+          post url, headers: auth_header(user), params: system_requirement_invalid_params    
         end.to_not change(SystemRequirement, :count)
       end
   
       it 'return error message' do
-        post url, headers: auth_header(user), params: system_requirements_invalid_params
+        post url, headers: auth_header(user), params: system_requirement_invalid_params
         expect(body_json['errors']['fields']).to have_key('name') 
       end
   
       it 'return unprocessable_entity status' do
-        post url, headers: auth_header(user), params: system_requirements_invalid_params
+        post url, headers: auth_header(user), params: system_requirement_invalid_params
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
@@ -72,7 +72,7 @@ RSpec.describe "Admin::V1::SystemRequirements as :admin", type: :request do
     let(:url) { "/admin/v1/system_requirements/#{system_requirement.id}" }
     
     context "with valid params" do
-      let(:new_name) { 'My updated System Requirements' }
+      let(:new_name) { 'My updated SystemRequirement' }
       let(:system_requirement_params) { { system_requirement: { name: new_name } }.to_json }
 
       it 'updates SystemRequirement' do
