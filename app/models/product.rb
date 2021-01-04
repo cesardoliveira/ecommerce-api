cd4e4e27b2356263fc8f3ab99b5ae3afeade7131
Cesar Oliveira <cesarsdoliveira@gmail.com>
2021-01-04 17:36:57 +1100

    Updated model Product and fixed validation tests in product_spec

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,10 +5,13 @@ class Product < ApplicationRecord
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :image, presence: true
-  
+  validates :status, presence: true
+
   has_many :product_categories, dependent: :destroy
   has_many :categories, through: :product_categories
   has_one_attached :image
+
+  enum status: { available: 1, unavailable: 2 }
 
   include NameSearchable
   include Paginatable
